ae14991a41fb2b77bc43e6d00d6a899db50cecaf
Smart Dev <developertouseeq@gmail.com>
2022-02-07 02:05:24 +0500

    Dev Touseeq //Login screen added with validations

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:event_booking_app/screens/register_screen/register_screen.dart';
+import 'package:event_booking_app/screens/user-module/login-page.dart';
 import 'package:event_booking_app/utils/constants/routes.dart';
 import 'package:flutter/material.dart';
-
 
 void main() {
   runApp(const MyApp());
@@ -13,8 +13,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      title: 'Flutter Demo',
-      home: RegisterScreen()
-    );
+        title: 'Flutter Demo',
+        debugShowCheckedModeBanner: false,
+        home: LoginPage());
   }
 }
